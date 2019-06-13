a0403e329e5dee823a609aede6dac4bbd8fa2318
jazz-code <ren.meta@gmail.com>
2019-06-13 15:28:45 -0400

    added Student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,34 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}`
     }
 }
+class Instructor extends Person {
+    constructor(personProps) {
+        super(personProps);
+        this.specialty = personProps.specialty;
+        this.favLanguage = personProps.favLanguage;
+        this.catchPhrase = personProps.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+class Student extends Person {
+    constructor(studentProps) {
+        super(studentProps);
+        this.previousBackground = studentProps.previousBackground;
+        this.className = studentProps.className;
+        this.favSubjects = studentProps.favSubjects;
+    }
+    listsSubjects() {
+        return `${this.favSubjects}`
+    }
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`
+    }
+}
